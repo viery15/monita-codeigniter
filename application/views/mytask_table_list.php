20802db8e10844fbd46f5bb3bac61a482649870d
Viery Darmawan <vierydarmawan2@gmail.com>
2019-02-27 11:44:32 +0700

    fix datatable

--- application/views/mytask_table_list.php
+++ application/views/mytask_table_list.php
@@ -82,113 +82,113 @@ $date_now = date('d M Y');
                 title: 'Tasks of '+ <?= $this->session->nik ?>+' (per <?= $date_now ?>)',
                 filename: 'Tasks of '+ <?= $this->session->nik ?>+' (per <?= $date_now ?>)',
             }],
-            "fnDrawCallback": function( oSettings ) {
-                $('.btn-delete').click(function(){
-                    var id = $(this).attr('id');
-                    if (confirm('Are you sure you want to delete this?')) {
-                        $.ajax({
-                            url: "<?php echo base_url(); ?>/Mytask/delete",
-                            type: 'post',
-                            data: {'id': id},
-                            success: function (a) {
-                                alert("Data deleted successful");
-                                $('#modal-task').modal('hide');
-                                $("#mytask-table-list").html(a);
-                            }
-                        });
+        });
+
+        $('.btn-delete').click(function(){
+            var id = $(this).attr('id');
+            if (confirm('Are you sure you want to delete this?')) {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>/Mytask/delete",
+                    type: 'post',
+                    data: {'id': id},
+                    success: function (a) {
+                        alert("Data deleted successful");
+                        $('#modal-task').modal('hide');
+                        $("#mytask-table-list").html(a);
                     }
                 });
+            }
+        });
 
-                $('.btn-approve').click(function(){
-                    var id = $(this).attr('id');
-                    if (confirm('Are you sure you want to approve this?')) {
-                        $.ajax({
-                            url: "<?php echo base_url(); ?>/Mytask/approve",
-                            type: 'post',
-                            data: {'id': id},
-                            beforeSend: function () {
-                                $('#loading').click();
-                            },
-                            success: function (a) {
-                                $('#modal-loading').modal('toggle');
-                                alert("Data approved successful");
-                                $("#mytask-table-list").html(a);
-                            }
-                        });
+        $("#example").on("click", ".btn-approve", function(){
+            var id = $(this).attr('id');
+            if (confirm('Are you sure you want to approve this?')) {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>/Mytask/approve",
+                    type: 'post',
+                    data: {'id': id},
+                    beforeSend: function () {
+                        $('#loading').click();
+                    },
+                    success: function (a) {
+                        $('#modal-loading').modal('toggle');
+                        alert("Data approved successful");
+                        $("#mytask-table-list").html(a);
                     }
                 });
+            }
+        });
 
-                $('.btn-done').click(function(){
-                    var id = $(this).attr('id');
-                    if (confirm('Are you sure done with this request?')) {
-                        $.ajax({
-                            url: "<?php echo base_url(); ?>/Mytask/done",
-                            type: 'post',
-                            data: {'id': id},
-                            beforeSend: function () {
-                                $('#loading').click();
-                            },
-                            success: function (a) {
-                                $('#modal-loading').modal('toggle');
-                                alert("Success");
-                                $("#mytask-table-list").html(a);
-                            }
-                        });
+        $("#example").on("click", ".btn-done", function(){
+            var id = $(this).attr('id');
+            if (confirm('Are you sure done with this request?')) {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>/Mytask/done",
+                    type: 'post',
+                    data: {'id': id},
+                    beforeSend: function () {
+                        $('#loading').click();
+                    },
+                    success: function (a) {
+                        $('#modal-loading').modal('toggle');
+                        alert("Success");
+                        $("#mytask-table-list").html(a);
                     }
                 });
+            }
+        });
 
-                $('.btn-reject').click(function(){
-                    var id = $(this).attr('id');
-                    if (confirm('Are you sure you want to reject this?')) {
-                        $.ajax({
-                            url: "<?php echo base_url(); ?>/Mytask/reject",
-                            type: 'post',
-                            data: {'id': id},
-                            beforeSend: function () {
-                                $('#loading').click();
-                            },
-                            success: function (a) {
-                                $('#modal-loading').modal('toggle');
-                                alert("Data rejected successful");
-                                $("#mytask-table-list").html(a);
-                            }
-                        });
+        $("#example").on("click", ".btn-reject", function(){
+            var id = $(this).attr('id');
+            if (confirm('Are you sure you want to reject this?')) {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>/Mytask/reject",
+                    type: 'post',
+                    data: {'id': id},
+                    beforeSend: function () {
+                        $('#loading').click();
+                    },
+                    success: function (a) {
+                        $('#modal-loading').modal('toggle');
+                        alert("Data rejected successful");
+                        $("#mytask-table-list").html(a);
                     }
                 });
+            }
+        });
 
-                $('.btn-comment').click(function(){
-                    var id = $(this).attr('id');
-                    $('#content-modal-comment').load("<?php echo base_url(); ?>/Myrequest/form_comment/"+id);
-                });
+        $("#example").on("click", ".btn-comment", function(){
+            var id = $(this).attr('id');
+            $('#content-modal-comment').load("<?php echo base_url(); ?>/Myrequest/form_comment/"+id);
+        });
 
-                $(".btn-cancel").click(function(){
-                    $(".close").click();
-                    var id_task = $(this).attr('id');
-                    var page = '';
-                    if (confirm('Are you sure you want to cancel this?')) {
-                        $.ajax({
-                            url: "<?php echo base_url(); ?>/Mytask/cancel",
-                            type: 'post',
-                            data: {
-                                'id': id_task,
-                                'page': page
-                            },
-                            beforeSend: function () {
-                                $('#loading').click();
-                            },
-                            success: function (a) {
-                                if (page == 'page detail') {
-                                    $("#task-content").html(a);
-                                }
-                                else {
-                                    $("#mytask-table-list").html(a);
-                                }
-                                $('#modal-loading').modal('toggle');
-                                alert("Data canceled successful");
-                            }
-                        });
+
+        $("#example").on("click", ".btn-cancel", function(){
+            $(".close").click();
+            var id_task = $(this).attr('id');
+            var page = '';
+            if (confirm('Are you sure you want to cancel this?')) {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>/Mytask/cancel",
+                    type: 'post',
+                    data: {
+                        'id': id_task,
+                        'page': page
+                    },
+                    beforeSend: function () {
+                        $('#loading').click();
+                    },
+                    success: function (a) {
+                        if (page == 'page detail') {
+                            $("#task-content").html(a);
+                        }
+                        else {
+                            $("#mytask-table-list").html(a);
+                        }
+                        $('#modal-loading').modal('toggle');
+                        alert("Data canceled successful");
                     }
-                })
+                });
             }
         });
     });
