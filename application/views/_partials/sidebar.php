0904f11ed5d4c33e755210584afe19b5850351ea
viery <vierydarmawan2@gmail.com>
2019-03-05 11:42:01 +0700

    grup menu admin

--- application/views/_partials/sidebar.php
+++ application/views/_partials/sidebar.php
@@ -7,6 +7,35 @@
     <div class="menu-sidebar__content js-scrollbar1">
         <nav class="navbar-sidebar">
             <ul class="list-unstyled navbar__list">
+                <?php
+                if ($this->session->role == 'admin') {
+                  if ($this->uri->segment(1) == 'category' || $this->uri->segment(1) == 'users' || $this->uri->segment(1) == 'manage_task') {
+                    $display = 'block';
+                  }
+                  else {
+                    $display = 'none';
+                  }
+                ?>
+                <li class="has-sub" >
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-cogs"></i>Manage <i class="pull-right fa fa-angle-down"></i></a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list" style="display:<?= $display ?>">
+                      <li class="<?php echo $this->uri->segment(1) == 'manage_task' ? 'active': '' ?>">
+                          <a href="<?php echo base_url(); ?>manage_task">
+                          <i class="fas fa-th-large"></i> All Task</a>
+                      </li>
+                      <li class="<?php echo $this->uri->segment(1) == 'category' ? 'active': '' ?>">
+                          <a href="<?php echo base_url(); ?>category">
+                          <i class="fas fa-tag"></i> Category</a>
+                      </li>
+                      <li class="<?php echo $this->uri->segment(1) == 'users' ? 'active': '' ?>">
+                          <a href="<?php echo base_url(); ?>users">
+                          <i class="fas fa-user"></i> Users</a>
+                      </li>
+                    </ul>
+                </li>
+                <?php } ?>
+
                 <li class="<?php echo $this->uri->segment(1) == 'dashboard' ? 'active': '' ?>">
                     <a href="<?php echo base_url(); ?>dashboard">
                     <i class="fas fa-tachometer-alt"></i>Dashboard</a>
@@ -25,22 +54,6 @@
                     <a href="<?php echo base_url(); ?>mycalendar">
                         <i class="fas fa-calendar-alt"></i> My Calendar</a>
                 </li>
-                <?php
-                if ($this->session->role == 'admin') {
-                ?>
-                <li class="<?php echo $this->uri->segment(1) == 'manage_task' ? 'active': '' ?>">
-                    <a href="<?php echo base_url(); ?>manage_task">
-                        <i class="fas fa-th-large"></i> Manage Task</a>
-                </li>
-                <li class="<?php echo $this->uri->segment(1) == 'category' ? 'active': '' ?>">
-                    <a href="<?php echo base_url(); ?>category">
-                    <i class="fas fa-tag"></i> Category</a>
-                </li>
-                <li class="<?php echo $this->uri->segment(1) == 'users' ? 'active': '' ?>">
-                    <a href="<?php echo base_url(); ?>users">
-                    <i class="fas fa-user"></i> Users</a>
-                </li>
-                <?php } ?>
             </ul>
         </nav>
     </div>
